8cf6d059acea0c58970d4632d1461bcb1f918296
DiegoCS06 <dcalvos@ucenfotec.ac.cr>
2025-05-24 08:58:52 -0600

    UI

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("BIENVENIDO AL MENÚ.");
+Console.WriteLine("Seleccione una opción: ");
+Console.WriteLine("1. Agregar Productos");
+Console.WriteLine("2. Listar Productos");
+Console.WriteLine("3. Salir del Programa");
+Console.WriteLine("3. Salir del Programa");
