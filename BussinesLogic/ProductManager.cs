f2cb853231071da24548f7fc7d90c8d6fd53fa3b
DiegoCS06 <dcalvos@ucenfotec.ac.cr>
2025-05-24 10:57:08 -0600

    Acceso Completo a Menu

--- BussinesLogic/ProductManager.cs
+++ BussinesLogic/ProductManager.cs
@@ -1,23 +1,42 @@
 ﻿using Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BussinesLogic
 {
-    internal class ProductManager
+    public class ProductManager
     {
+        private List<Product> products = new List<Product>();
+
         public void AddProduct()
         {
+            Console.WriteLine("Ingrese la ID del Producto a añadir: ");
+            int idProducto = int.Parse(Console.ReadLine());
 
+            Console.WriteLine("Ingrese el nombre del Producto a añadir: ");
+            string nombreProducto = Console.ReadLine();
+
+            Console.WriteLine("Ingrese el precio del Producto a añadir: ");
+            double precioProducto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el stock del Producto a añadir: ");
+            int stockProducto = int.Parse(Console.ReadLine());
+
+            var producto = new Product
+            {
+                Id = idProducto,
+                Name = nombreProducto,
+                Price = precioProducto,
+                Stock = stockProducto
+            };
+
+            products.Add(producto);
+            Console.WriteLine("Producto añadido correctamente.\n");
         }
 
         public List<Product> GetProducts()
         {
-            var list = new List<Product>();
-            return list;
+            return products;
         }
     }
 }
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,32 +1,56 @@
-﻿public class Program {
+﻿using BussinesLogic;
+using System;
 
+public class Program
+{
     public static void Main(string[] args)
     {
+        var manager = new ProductManager(); // ✅ UNA instancia que guarda la lista
+        bool salir = false;
 
-        Console.WriteLine("BIENVENIDO AL MENÚ.");
-        Console.WriteLine("Seleccione una opción: ");
-        Console.WriteLine("1. Agregar Productos");
-        Console.WriteLine("2. Listar Productos");
-        Console.WriteLine("3. Salir del Programa");
-
-        var option=Int32.Parse(Console.ReadLine());
-
-        switch (option) 
+        while (!salir)
         {
-            case 1:
-                Console.WriteLine("Digite la información del Producto a añadir.");
-                break;
+            Console.WriteLine("BIENVENIDO AL MENÚ.");
+            Console.WriteLine("Seleccione una opción: ");
+            Console.WriteLine("1. Agregar Productos");
+            Console.WriteLine("2. Listar Productos");
+            Console.WriteLine("3. Salir del Programa");
 
-            case 2:
-                Console.WriteLine("Listado de Productos existentes: ");
-                break;
+            if (!int.TryParse(Console.ReadLine(), out int option))
+            {
+                Console.WriteLine("Opción inválida.\n");
+                continue;
+            }
 
-            case 3:
-                Console.WriteLine("Saliendo del Programa.");
-                break;
+            switch (option)
+            {
+                case 1:
+                    manager.AddProduct();
+                    break;
 
-            default: Console.WriteLine("Ingrese una opción válida.");
-                break;
+                case 2:
+                    var productos = manager.GetProducts();
+                    if (productos.Count == 0)
+                    {
+                        Console.WriteLine("No hay productos registrados.\n");
+                    }
+                    else
+                    {
+                        foreach (var producto in productos)
+                        {
+                            Console.WriteLine($"ID: {producto.Id}, Nombre: {producto.Name}, Precio: {producto.Price}, Stock: {producto.Stock}\n");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    salir = true;
+                    break;
+
+                default:
+                    Console.WriteLine("Ingrese una opción válida.\n");
+                    break;
+            }
         }
-    } 
+    }
 }
